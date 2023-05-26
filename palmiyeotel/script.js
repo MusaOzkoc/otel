06684b88f1ad2b87b32af98fe31088f735e70065
MusaOzkoc <musaozkoc3@gmail.com>
2023-05-26 23:07:14 +0300

    palmiyeotel

---
+++ palmiyeotel/script.js
@@ -0,0 +1,17 @@
+function uyari(){
+
+    alert("Bilgileriniz Kaydelildi.")
+
+}
+
+// Yukarıda adı yazan metodu tanımladım ve butona basıldığında uyarı vermesi için tanımladım//
+
+function kaydetuyari(){
+
+    alert("Erken Rezervasyon İçin Bilgiler Sağlandı.")
+
+}
+
+// Yukarıda adı yazan metodu tanımladım ve butona basıldığında uyarı vermesi için tanımladım//
+
+
